8c5f41bf3b5f67daa833dfc89049bd20eaad9aa6
Denis Eneotescu <denis.eneotescu@nttdata.ro>
2021-03-09 11:39:01 +0200

    initial commit

---
+++ src/main/java/ro/ubb/cloud/iParking/IParkingApplication.java
@@ -0,0 +1,13 @@
+package ro.ubb.cloud.iParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/ubb/cloud/iParking/IParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ubb.cloud.iParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
